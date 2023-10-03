67f7e78c137a605b019c0f811c3b796f69deb31c
Pingu Lee <137541277+PinguLee@users.noreply.github.com>
2023-10-03 14:34:03 +0900

    var -> const

--- Develop-Training-1.js
+++ Develop-Training-1.js
@@ -1,4 +1,4 @@
-var obj = {
+const obj = {
   academyName: "그린컴퓨터아트학원",
   location: "둔산점",
   address: "대전광역시 서구 대덕대로 182 오라클빌딩 3층",
@@ -28,7 +28,7 @@ var obj = {
 
 for(i = 0; i < obj.studentList.length; i++) {
   if(obj.studentList[i] === "이민구") {
-    console.log("이민구는 " + (i + 1) + "번에 있습니다.");
+    console.log(`이민구는 ${i + 1}번에 있습니다.`);
   }
 }
 console.log(obj.address);
