b8d6074ee15fd9d96d1570feb5580c9cd2a38a23
leemingoo <dwmapi@naver.com>
2023-10-04 19:02:23 +0900

    파일 쓰기

--- Develop-Training-12.js
+++ Develop-Training-12.js
@@ -4,24 +4,46 @@ const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
 
-rl.question("입력해주세요 : ", function(answer) {
-  console.log(`답변 : ${answer}`);
+let name;
+
+rl.question("이름 입력 : ", function (answer) {
+  console.log(`${answer}님 반갑습니다.`);
+  name = answer;
   rl.close();
 });
 
-http.createServer(function(request, response) {
-  console.log(request.method);
-  console.log(request.url);
+let html = `<!DOCTYPE html>
+  <html lang=\"ko\">
+  <head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>
+      ${name}님 반갑습니다.
+    </h1>
+  </body>
+  </html>`;
 
+fs.writeFile('./public/index.html', html, (err) => {
+  if (err) {
+    console.log('에러 : ', err);
+  }
+});
+
+
+http.createServer(function (request, response) {
   let writeHeadObject = {
     'Content-Type': 'text/html'
   }
   response.writeHead(200, writeHeadObject);
 
-  fs.readFile("./public/index.html", function(err, data) {
+  fs.readFile("./public/index.html", function (err, data) {
     if (err) {
       console.error("파일을 읽지 못했습니다.");
-    }else {
+    } else {
       response.end(data);
     }
   });
